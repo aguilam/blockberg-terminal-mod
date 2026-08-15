6a964d6c964ceb319400dba8978ad4f6656895e4
Aguilam <pexfex@mail.ru>
2026-08-15 14:26:33 +0400

    feat(neoforge): add mod init on neoforge

--- neoForge/src/main/java/com/aguilam/blockberg_terminal/BlockbergTerminal.java
+++ neoForge/src/main/java/com/aguilam/blockberg_terminal/BlockbergTerminal.java
@@ -1,5 +1,15 @@
 package com.aguilam.blockberg_terminal;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
+
+@Mod(BlockbergTerminal.MOD_ID)
 public class BlockbergTerminal {
-    
+    public static final String MOD_ID = "blockberg_terminal";
+    public BlockbergTerminal(IEventBus modEventBus) {
+        if(FMLEnvironment.dist.isClient()){
+            BlockbergTerminalClient.init();
+        }
+    }
 }
---
+++ neoForge/src/main/java/com/aguilam/blockberg_terminal/BlockbergTerminalClient.java
@@ -0,0 +1,51 @@
+package com.aguilam.blockberg_terminal;
+import com.aguilam.blockberg_terminal.commands.Barrel;
+import com.aguilam.blockberg_terminal.commands.Regions;
+import com.aguilam.blockberg_terminal.config.ConfigManager;
+import com.aguilam.blockberg_terminal.feature.ProcessStorageScreen;
+import com.aguilam.blockberg_terminal.local.LocalServer;
+import com.aguilam.blockberg_terminal.region.RegionsManager;
+import com.aguilam.blockberg_terminal.render.RenderBlocks;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import net.neoforged.neoforge.client.event.ScreenEvent;
+import net.neoforged.neoforge.common.NeoForge;
+
+public class BlockbergTerminalClient {
+    public static void init() {
+        ConfigManager.file = FMLPaths.CONFIGDIR.get().resolve("blockberg-terminal.json").toFile();
+        LocalServer.gameDir = FMLPaths.GAMEDIR.get();
+        RegionsManager.loadRegions();
+        ConfigManager.load();
+
+        NeoForge.EVENT_BUS.addListener((ScreenEvent.Init.Post event) -> {
+            ProcessStorageScreen.checkStorageScreen(event.getScreen(), Minecraft.getInstance());
+        });
+
+        NeoForge.EVENT_BUS.addListener((ClientTickEvent.Post event) -> {
+            ProcessStorageScreen.checkStorageTick();
+        });
+        
+        NeoForge.EVENT_BUS.addListener((RegisterClientCommandsEvent event) -> {
+            var dispatcher = event.getDispatcher();
+            dispatcher.register(Barrel.searchBarrels());
+            dispatcher.register(Barrel.showBarrelContent());
+            dispatcher.register(Regions.setMin());
+            dispatcher.register(Regions.setMax());
+            dispatcher.register(Regions.addRegion());
+            dispatcher.register(Regions.allRegions());
+            dispatcher.register(Regions.scan());
+            dispatcher.register(Regions.clearHighlighted());
+        });
+
+        NeoForge.EVENT_BUS.addListener((RenderLevelStageEvent event) -> {
+            if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT_BLOCKS) {
+                RenderBlocks.renderHighlightedBlocks(event.getPoseStack());
+            }
+        });
+    }
+}
