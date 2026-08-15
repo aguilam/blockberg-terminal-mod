e5b2264a538e7d29c97064a08c5e4026a02212f3
Aguilam <pexfex@mail.ru>
2026-08-15 15:31:43 +0400

    feat(common,fabric,neoforge): move config screen factory to common and add config screen to neoforge

--- neoForge/src/main/java/com/aguilam/blockberg_terminal/BlockbergTerminal.java
+++ neoForge/src/main/java/com/aguilam/blockberg_terminal/BlockbergTerminal.java
@@ -1,15 +1,16 @@
 package com.aguilam.blockberg_terminal;
 
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod(BlockbergTerminal.MOD_ID)
 public class BlockbergTerminal {
     public static final String MOD_ID = "blockberg_terminal";
-    public BlockbergTerminal(IEventBus modEventBus) {
+    public BlockbergTerminal(ModContainer modContainer, IEventBus modEventBus) {
         if(FMLEnvironment.dist.isClient()){
-            BlockbergTerminalClient.init();
+            BlockbergTerminalClient.init(modContainer);
         }
     }
 }
--- neoForge/src/main/java/com/aguilam/blockberg_terminal/BlockbergTerminalClient.java
+++ neoForge/src/main/java/com/aguilam/blockberg_terminal/BlockbergTerminalClient.java
@@ -2,21 +2,24 @@ package com.aguilam.blockberg_terminal;
 import com.aguilam.blockberg_terminal.commands.Barrel;
 import com.aguilam.blockberg_terminal.commands.Regions;
 import com.aguilam.blockberg_terminal.config.ConfigManager;
+import com.aguilam.blockberg_terminal.config.ConfigScreen;
 import com.aguilam.blockberg_terminal.feature.ProcessStorageScreen;
 import com.aguilam.blockberg_terminal.local.LocalServer;
 import com.aguilam.blockberg_terminal.region.RegionsManager;
 import com.aguilam.blockberg_terminal.render.RenderBlocks;
 
 import net.minecraft.client.Minecraft;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.event.ScreenEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 
 public class BlockbergTerminalClient {
-    public static void init() {
+    public static void init(ModContainer modContainer) {
         ConfigManager.file = FMLPaths.CONFIGDIR.get().resolve("blockberg-terminal.json").toFile();
         LocalServer.gameDir = FMLPaths.GAMEDIR.get();
         RegionsManager.loadRegions();
@@ -47,5 +50,7 @@ public class BlockbergTerminalClient {
                 RenderBlocks.renderHighlightedBlocks(event.getPoseStack());
             }
         });
+
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, (container, parentScreen) -> ConfigScreen.createConfigScreen(parentScreen));
     }
 }
